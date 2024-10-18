8589795b712c8eaf50ed1676471c2183903cc2c0
ds-upin <divyanshu.singh.bzzf@gmail.com>
2024-10-18 22:04:36 +0530

    modified middleware for better visiual in log

--- middleware/urlm.js
+++ middleware/urlm.js
@@ -5,7 +5,7 @@ const filepath = path.join(__dirname, '../log.txt');
 
 async function log_middleware(req, res, next) {
     const now = new Date();
-    const logEntry = `\nIP: ${req.ip}      ---URL: ${req.url}      ---TIMESTAMP: ${now.toISOString()}      ---METHOD: ${req.method}`;
+    const logEntry = `\nIP: ${req.ip}      ---TIMESTAMP: ${now.toISOString()}      ---METHOD: ${req.method}      ---URL: ${req.url}`;
 
     try {
         await fs.promises.appendFile(filepath, logEntry);
